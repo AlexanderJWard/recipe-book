7c0c8d3f5d51b6a352f6e1f7f368cbba1673ca8c
AlexanderJWard <alexjamesward@hotmail.com>
2023-04-25 23:17:24 +0000

    add custom hook for toggling navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,22 @@
 import styles from "../styles/NavBar.module.css";
-import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import logo from "../assets/logo.png";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../contexts/CurrentUserContext";
 import Avatar from "./Avatar";
 import axios from "axios";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
+
+  const { expanded, setExpanded, ref } = useClickOutsideToggle()
 
   const handleSignOut = async () => {
     try {
@@ -54,11 +59,7 @@ const NavBar = () => {
       >
         <i class="fa-solid fa-circle-question"></i> Support
       </NavLink>
-      <NavLink
-        to="/"
-        className={styles.NavLink}
-        onClick={handleSignOut}
-      >
+      <NavLink to="/" className={styles.NavLink} onClick={handleSignOut}>
         <i class="fa-solid fa-right-from-bracket"></i> Sign Out
       </NavLink>
       <NavLink
@@ -90,32 +91,39 @@ const NavBar = () => {
   );
 
   return (
-    <div>
-      <Navbar className={styles.NavBar} expand="md" fixed="top">
-        <Container>
-          <NavLink to="/">
-            <Navbar.Brand>
-              <img src={logo} alt="logo" height="45" /> Recipe Book
-            </Navbar.Brand>
-          </NavLink>
-          {currentUser && addRecipeIcon}
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto text-left">
-              <NavLink
-                exact
-                to="/"
-                className={styles.NavLink}
-                activeClassName={styles.Active}
-              >
-                <i class="fa-solid fa-house-chimney"></i> Home
-              </NavLink>
-              {currentUser ? loggedInIcons : loggedOutIcons}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
+    <Navbar
+      expanded={expanded}
+      className={styles.NavBar}
+      expand="md"
+      fixed="top"
+    >
+      <Container>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="45" /> Recipe Book
+          </Navbar.Brand>
+        </NavLink>
+        {currentUser && addRecipeIcon}
+        <Navbar.Toggle
+          ref={ref}
+          onClick={() => setExpanded(!expanded)}
+          aria-controls="basic-navbar-nav"
+        />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <NavLink
+              exact
+              to="/"
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+            >
+              <i class="fa-solid fa-house-chimney"></i> Home
+            </NavLink>
+            {currentUser ? loggedInIcons : loggedOutIcons}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
