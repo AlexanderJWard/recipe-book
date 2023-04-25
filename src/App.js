4421c617bb1ddf97d27451718cd7a1534554d691
AlexanderJWard <alexjamesward@hotmail.com>
2023-04-25 22:09:59 +0000

    add UseContext to add ternary to NavBar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import Container from 'react-bootstrap/Container';
-import styles from './App.module.css';
-import NavBar from './components/NavBar';
-import {Route,Switch} from 'react-router-dom';
-import './api/axiosDefaults';
-import SignUpForm from './pages/auth/SignUpForm';
-import SignInForm from './pages/auth/SignInForm';
+import Container from "react-bootstrap/Container";
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
+import { Route, Switch } from "react-router-dom";
+import "./api/axiosDefaults";
+import axios from "axios";
+import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForm";
+import { createContext, useEffect, useState } from "react";
+
+export const CurrentUserContext = createContext();
+export const SetCurrentUserContext = createContext();
 
 function App() {
+  // #FFB677
+  // #917059
 
-// #FFB677
-// #917059
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const handleMount = async () => {
+    try {
+      const { data } = await axios.get("dj-rest-auth/user/");
+      setCurrentUser(data);
+    } catch (err) {
+      // console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    handleMount();
+  }, []);
 
   return (
-    <div className={styles.App}>
-      <NavBar />
-      <Container className={styles.Main}>
-        <Switch>
-          <Route exact path="/" render={() => <h1>Home page</h1>} />
-          <Route exact path="/signin" render={() => <SignInForm />} />
-          <Route exact path="/signup" render={() => <SignUpForm />} />
-          <Route render={() =><p>Page not found!</p>} />
-        </Switch>
-      </Container>
-    </div>
+    <CurrentUserContext.Provider value={currentUser}>
+      <SetCurrentUserContext.Provider value={setCurrentUser}>
+        <div className={styles.App}>
+          <NavBar />
+          <Container className={styles.Main}>
+            <Switch>
+              <Route exact path="/" render={() => <h1>Home page</h1>} />
+              <Route exact path="/signin" render={() => <SignInForm />} />
+              <Route exact path="/signup" render={() => <SignUpForm />} />
+              <Route render={() => <p>Page not found!</p>} />
+            </Switch>
+          </Container>
+        </div>
+      </SetCurrentUserContext.Provider>
+    </CurrentUserContext.Provider>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,34 @@
 import styles from "../styles/NavBar.module.css";
-import React from "react";
+import React, { useContext } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import logo from "../assets/logo.png";
 import { NavLink } from "react-router-dom";
+import { CurrentUserContext } from "../App";
 
 const NavBar = () => {
+  const currentUser = useContext(CurrentUserContext);
+  const loggedInIcons = <>{currentUser?.username}</>
+  const loggedOutIcons = (
+    <>
+      <NavLink
+        to="/signin"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        <i class="fa-solid fa-right-to-bracket"></i> Sign In
+      </NavLink>
+      <NavLink
+        to="/signup"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        <i class="fa-solid fa-user-plus"></i> Sign Up
+      </NavLink>
+    </>
+  );
+
   return (
     <div>
       <Navbar className={styles.NavBar} expand="md" fixed="top">
@@ -19,15 +41,15 @@ const NavBar = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto text-left">
-              <NavLink exact to="/" className={styles.NavLink} activeClassName={styles.Active}>
+              <NavLink
+                exact
+                to="/"
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+              >
                 <i class="fa-solid fa-house-chimney"></i> Home
               </NavLink>
-              <NavLink to="/signin" className={styles.NavLink} activeClassName={styles.Active}>
-                <i class="fa-solid fa-right-to-bracket"></i> Sign In
-              </NavLink>
-              <NavLink to="/signup" className={styles.NavLink} activeClassName={styles.Active}>
-                <i class="fa-solid fa-user-plus"></i> Sign Up
-              </NavLink>
+              {currentUser ? loggedInIcons : loggedOutIcons}
             </Nav>
           </Navbar.Collapse>
         </Container>
