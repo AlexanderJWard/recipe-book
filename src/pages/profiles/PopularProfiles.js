8faa66bb63b4008c8576814ea192ca81734ed59d
AlexanderJWard <alexjamesward@hotmail.com>
2023-04-26 22:57:57 +0000

    Add Profile and ProfilePage

---
+++ src/pages/profiles/PopularProfiles.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Loading from "../../components/Loading";
+import { useProfileData } from "../../contexts/ProfileDataContext";
+import Profile from "./Profile";
+
+const PopularProfiles = ({ mobile }) => {
+    const { popularProfiles } = useProfileData();
+
+  return (
+    <Container
+      className={`${
+        mobile && "d-lg-none text-center mb-3"
+      }`}
+    >
+      {popularProfiles.results.length ? (
+        <>
+          <p>Most followed profiles.</p>
+          {mobile ? (
+            <div className="d-flex justify-content-around">
+              {popularProfiles.results.slice(0, 4).map((profile) => (
+                <Profile key={profile.id} profile={profile} mobile />
+              ))}
+            </div>
+          ) : (
+            popularProfiles.results.map((profile) => (
+              <Profile key={profile.id} profile={profile} />
+            ))
+          )}
+        </>
+      ) : (
+        <Loading spinner />
+      )}
+    </Container>
+  );
+};
+
+export default PopularProfiles;
